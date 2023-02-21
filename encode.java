ff8c95bb034b47484fcc17eaeb246c50fb4e8ec9
paras21avenue <92352193+paras21avenue@users.noreply.github.com>
2023-02-22 03:31:26 +0530

    Update encode.java

--- encode.java
+++ encode.java
@@ -2,11 +2,7 @@ public class encode
 {  
 public static void main(String args[])  
 {  
-//character array  
-char[] ch = {'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ' , 't', 'o', ' ', 'J', 'a', 'v', 'a', 't', 'p', 'o', 'i', 'n', 't'};  
-//constructor of the String class that parses char array as a parameter  
-String string = new String(ch);  
-//prints the string  
-System.out.println(string);  
+String test= "abcd";
+System.out.println(test);  
 }  
 } 
