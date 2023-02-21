ff8a3c6c356c5e6fae8d25a30bbb44b39ac2c1ec
paras21avenue <92352193+paras21avenue@users.noreply.github.com>
2023-02-22 03:26:36 +0530

    Update encode.java

--- encode.java
+++ encode.java
@@ -1,23 +1,11 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Base64;
-
-
-
-public class testsubstring {
-	
-	public static void main (String [] args) throws IOException
-	{
-	
-        	 String text = new String(Files.readAllBytes(Paths.get("test.vql")), StandardCharsets.UTF_8);
-	String encodedString = Base64.getEncoder().encodeToString(text.getBytes());
-	System.out.println(encodedString);
-		
-		 }  
-	}
+public class CharArrayToStringExample  
+{  
+public static void main(String args[])  
+{  
+//character array  
+char[] ch = {'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ' , 't', 'o', ' ', 'J', 'a', 'v', 'a', 't', 'p', 'o', 'i', 'n', 't'};  
+//constructor of the String class that parses char array as a parameter  
+String string = new String(ch);  
+//prints the string  
+System.out.println(string);  
+}  
