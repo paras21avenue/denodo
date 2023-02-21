57224900d1ff67899c14913dfe3be1c4edc26b71
paras21avenue <92352193+paras21avenue@users.noreply.github.com>
2023-02-22 03:13:16 +0530

    Create encode.java
    encode

---
+++ encode.java
@@ -0,0 +1,25 @@
+import static io.restassured.RestAssured.given;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+
+
+public class testsubstring {
+	
+	public static void main (String [] args) throws IOException
+	{
+	
+        	 String text = new String(Files.readAllBytes(Paths.get("C:\\\\Users\\\\paras_pokhrel\\\\Downloads\\\\Revision96-VQL-2022-10-26.vql")), StandardCharsets.UTF_8);
+	String encodedString = Base64.getEncoder().encodeToString(text.getBytes());
+	System.out.println(encodedString);
+		
+		 }  
+	}
