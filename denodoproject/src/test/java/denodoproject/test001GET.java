a545c8fb65ca590eb822231eff13cc28bad1a745
paras21avenue <92352193+paras21avenue@users.noreply.github.com>
2023-04-15 01:03:53 +0530

    Update test001GET.java

--- denodoproject/src/test/java/denodoproject/test001GET.java
+++ denodoproject/src/test/java/denodoproject/test001GET.java
@@ -1,29 +1,25 @@
 package denodoproject;
 
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.json.JSONArray;
 import org.json.simple.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
 
 public class test001GET {
+	
 	@Test
 	public void validate_response_status_code_403() {
 	    
@@ -34,6 +30,5 @@ public class test001GET {
 	    Response response = httpRequest.get("/tov5c2VC2c1RKXeM80rCtgXVmGN6Kj");
 	    Assert.assertEquals(403, response.getStatusCode());
 	}
-	
-	
+
 }
